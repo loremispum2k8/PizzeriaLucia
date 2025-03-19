f416f354e21ea600ca90d467eafafeb8c3f151c3
Loremispum <141552551+loremispum2k8@users.noreply.github.com>
2025-03-20 00:09:14 +0200

    Fix the scroll on popup

--- script.js
+++ script.js
@@ -7,13 +7,17 @@ let exitMenu = document.getElementsByClassName('closeMenu');
 menuBurger[0].addEventListener('click',()=>{
     menu[0].classList.remove('hideMenu');
     menu[0].classList.add('showMenu');
+    menu[0].style.display = 'flex';
     darkBackground[0].classList.remove('hide');
     darkBackground[0].classList.add('show');
     darkBackground[0].style.display = 'block';
+    document.body.classList.add('stop-scroll');
 })
 exitMenu[0].addEventListener('click',()=>{
     menu[0].classList.add('hideMenu');
     menu[0].classList.remove('showMenu');
+    menu[0].style.display = 'none';
     darkBackground[0].classList.add('hide');
     darkBackground[0].classList.remove('show');
+    document.body.classList.remove('stop-scroll');
 })
